20c9d7af10094f652c1c9d2d6cb1ad0568dc8664
James Britton <brittonjp@gmail.com>
2022-01-11 20:29:04 +0000

    added basic routes, question_controller and ask & answer views plus coach responses

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+require 'byebug'
+
+class QuestionsController < ApplicationController
+
+  def ask
+
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = 'I am going to work'
+    if @question.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif @question == 'I am going to work'
+      @answer = 'Great!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
+    end
+  end
+end
